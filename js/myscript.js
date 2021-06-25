6a938d1af19a4dd773d78bb0e22dbf16e145ca61
2845815262 <2845815262@qq.com>
2021-06-25 09:38:28 +0800

    ooklibaioo

--- js/myscript.js
+++ js/myscript.js
@@ -4,12 +4,4 @@ let Pics = ['../img/bg/p0.jpg',
             '../img/bg/p3.jpg']
     
 var n = 0;
-function myFunction() {
-    if (n <= 0) {
-        n++;
-        console.log(n);
-    } else if (n = Pics.length-1) {
-        n--;
-        console.log(n);
-    }
-}
+function myFunction(i)

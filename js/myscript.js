a244453e2fc1ba438e952a793f3f682832573944
moran <ooklibaioo@gmail.com>
2023-06-11 00:11:56 +0800

    new

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,6 @@
 $(function (){
 
 let wall = "";
-let firstw = '<div id="wall"><div id="wall2"></div></div>';
 
 for (let i = 0; i < 40; i++) {
     wall += '<div id="wall"><div id="wall2" style="background-image: url(../../imgs/Wallpaper/'+i+'.jpg);background-size: cover;background-position: center;"></div></div>';

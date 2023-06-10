19a593eabbb33fbab243b850a1c6b273fe0750dc
moran <ooklibaioo@gmail.com>
2023-06-11 00:27:32 +0800

    new

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,7 @@ $(function (){
 let wall = "";
 
 for (let i = 0; i < 40; i++) {
-    wall += '<div id="wall"><div id="wall2" style="background-image: url(../../imgs/Wallpaper/'+i+'.jpg);background-size: cover;background-position: center;"></div></div>';
+    wall += '<a href = "../../imgs/Wallpaper/'+i+'.jpg"><div id="wall"><div id="wall2" style="background-image: url(../../imgs/Wallpaper/'+i+'.jpg);background-size: cover;background-position: center;"></div></div></a>';
 }
 
 document.getElementById("wall1").innerHTML = wall;

9573d826240292f06688598af52f7e0b0d6ebe7a
moran <ooklibaioo@gmail.com>
2023-06-12 08:16:33 +0800

    new

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,7 @@ $(function () {
     let wall = "";
 
     for (let i = 0; i < 40; i++) {
-        wall += '<a href = "../../imgs/Wallpaper/' + i + '.jpg"><div id="wall"><div id="wall2" style="background-image: url(../../imgs/Wallpaper/' + i + '.jpg);background-size: cover;background-position: center;"><div id="wall3"><p>点击预览</p></div></div></div></a>';
+        wall += '<a download = "../../imgs/Wallpaper/' + i + '.jpg"><div id="wall"><div id="wall2" style="background-image: url(../../imgs/Wallpaper/' + i + '.jpg);background-size: cover;background-position: center;"><div id="wall3"><p>点击下载</p></div></div></div></a>';
     }
 
     document.getElementById("wall1").innerHTML = wall;

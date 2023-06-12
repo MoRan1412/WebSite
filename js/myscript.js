4c0140af9388fade495e7e56e4ac15504da3263f
moran <ooklibaioo@gmail.com>
2023-06-12 08:41:03 +0800

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,7 @@ $(function () {
     let wall = "";
 
     for (let i = 0; i < 40; i++) {
-        wall += '<a download = "../../imgs/Wallpaper/' + i + '.jpg" download><div id="wall"><div id="wall2" style="background-image: url(../../imgs/Wallpaper/' + i + '.jpg);background-size: cover;background-position: center;"><div id="wall3"><p>点击下载</p></div></div></div></a>';
+        wall += '<a href = "../../imgs/Wallpaper/' + i + '.jpg" download = "' + i + '.jpg"><div id="wall"><div id="wall2" style="background-image: url(../../imgs/Wallpaper/' + i + '.jpg);background-size: cover;background-position: center;"><div id="wall3"><p>点击下载</p></div></div></div></a>';
     }
 
     document.getElementById("wall1").innerHTML = wall;

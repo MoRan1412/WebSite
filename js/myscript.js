0f36a95f3c79bee4832153e81ed8c3bb41c110b3
moran <ooklibaioo@gmail.com>
2023-06-13 23:20:02 +0800

    wall

--- js/myscript.js
+++ js/myscript.js
@@ -7,10 +7,9 @@ $(function () {
     for (let i = 0; i < 40; i++) {
 
         photo = i;
-        link = 'https://www.ooklibaioo.com/imgs/Wallpaper/' + photo + '.jpg';
-        download = "window.location.href ='" + link + "'";
+        link = '../../imgs/Wallpaper/' + photo + '.jpg';
 
-        wall += '<a href="javascript:void(0);" onclick="' + download + ';return false;"><div id="wall"><div id="wall2" style="background-image: url(' + link + ');background-size: cover;background-position: center;"><div id="wall3"></div></div></div></a>';
+        wall += '<a href = "' + link + '" download="" target="_blank"><div id="wall"><div id="wall2" style="background-image: url(' + link + ');background-size: cover;background-position: center;"><div id="wall3"><p>点击下载</p></div></div></div></a>';
     }
 
     document.getElementById("wall1").innerHTML = wall;

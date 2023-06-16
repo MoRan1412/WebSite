b5b83e2a9f73197d3a221586e6f1de2a2bef59c8
moran <ooklibaioo@gmail.com>
2023-06-16 16:04:23 +0800

    silk

--- js/myscript.js
+++ js/myscript.js
@@ -9,9 +9,9 @@ $(function () {
         photo = i;
         link = '../../imgs/Wallpaper/' + photo + '.jpg';
 
-        click = "location.href='"+link+"';";
+        click = "location.href='" + link + "';";
 
-        wall += '<div id="wall" onclick="'+click+'return false;"><div id="wall2" style="background-image: url(' + link + ');background-size: cover;background-position: center;"><div id="wall3"></div></div></div>';
+        wall += '<div id="wall" onclick="' + click + 'return false;"><div id="wall2" style="background-image: url(' + link + ');background-size: cover;background-position: center;"><div id="wall3"></div></div></div>';
     }
 
     document.getElementById("wall1").innerHTML = wall;
@@ -44,3 +44,11 @@ function empty() {
 function harmonymm() {
     window.location.href = "https://www.ooklibaioo.com/apk/Harmony3_0Manager.apk";
 }
+
+
+$.getJSON('https://raw.githubusercontent.com/MoRan1412/KLWP-Data/main/Update/Silk.json', function (data) {
+
+    var text = `v${data.version}`
+
+    $(".silkver").html(text);
+});

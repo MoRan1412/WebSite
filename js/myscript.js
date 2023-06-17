0f3d36add4e69bfab04e3ccc2002332f7dc2ef88
moran <ooklibaioo@gmail.com>
2023-06-17 20:02:59 +0800

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -20,7 +20,7 @@ $(function () {
 
 
 function klwppro() {
-    window.location.href = "http://zuoye.free.fr/files/KLWP_Pro.apk";
+    window.location.href = "https://www.ooklibaioo.com/apk/KLWP_Pro.apk";
 }
 
 function klwp() {
@@ -40,7 +40,7 @@ function novab() {
 }
 
 function empty() {
-    window.location.href = "http://zuoye.free.fr/files/EmptyW.apk";
+    window.location.href = "https://www.ooklibaioo.com/apk/EmptyW.apk";
 }
 
 function harmonymm() {
